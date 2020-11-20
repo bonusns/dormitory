a03308e86f70be02f01882b1d3c0bec599cee891
bonusns <bonyshd@gmail.com>
2020-11-20 15:09:32 +0300

    test

--- massivi.py
+++ massivi.py
@@ -10,14 +10,7 @@ while chislo_rabocih > 0:
 
 chislo_rabocih = 10
 
-for nomer_rabochigo in range(0,chislo_rabocih):
-    if kolvo_detaley[nomer_rabochigo] >= 1000:
-        print(f"Рабочий под номером {nomer_rabochigo+1} получает зп")
-        if 1001 <= kolvo_detaley[nomer_rabochigo] <= 1500:
-            print("Премия +1000\n")
-        elif 1501 <= kolvo_detaley[nomer_rabochigo] <= max:
-            print("Премия +2000\n")
-        else:
-            print()
-    else:
-        print(f"Дядя под номером {nomer_rabochigo+1} ты в этом месяце без зп\n")
+for i in range(10):
+    print('Sasha pochini github')
+
+    print('On ne hochet rabotat')
