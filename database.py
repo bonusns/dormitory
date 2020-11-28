49cb2a7241e610cc5a0148b83f91f5949e531088
bonusns <bonyshd@gmail.com>
2020-11-28 16:07:35 +0300

    database – функции бд dormitory_init создает общаги

--- database.py
+++ database.py
@@ -6,23 +6,24 @@ def noquote(s):
     return s
 pyrebase.pyrebase.quote = noquote
 
-firebaseConfig = {'apiKey': "AIzaSyCcFUTbszDhbcZxCuXqM85MWr80FPhvY58",
-   'authDomain': "dormitory-c5829.firebaseapp.com",
-    'databaseURL': "https://dormitory-c5829.firebaseio.com",
-    'projectId': "dormitory-c5829",
-    'storageBucket': "dormitory-c5829.appspot.com",
-    'messagingSenderId': "282371830331",
-    'appId': "1:282371830331:web:c8c5c754d20184a045221e",
-    'measurementId': "G-QZSM07QZ73"}
 
-db = pyrebase.initialize_app(firebaseConfig).database()
 
-# def init_firebase(firebaseConfig):
-#
-#     return firebase.database()
+
+def init_firebase():
+    firebaseConfig = {'apiKey': "AIzaSyCcFUTbszDhbcZxCuXqM85MWr80FPhvY58",
+                      'authDomain': "dormitory-c5829.firebaseapp.com",
+                      'databaseURL': "https://dormitory-c5829.firebaseio.com",
+                      'projectId': "dormitory-c5829",
+                      'storageBucket': "dormitory-c5829.appspot.com",
+                      'messagingSenderId': "282371830331",
+                      'appId': "1:282371830331:web:c8c5c754d20184a045221e",
+                      'measurementId': "G-QZSM07QZ73"}
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    return firebase.database()
 
 # функции общежития
 def add_dormitory(number,address):
+    db = init_firebase()
     dormitoryData = {'number': number, 'Rooms': {0:''}, 'name': 'Общежитие ' + str(number), 'Адрес':address}
     db.child('dormitory' + str(number)).set(dormitoryData)
 
@@ -38,6 +39,7 @@ def update_number_of_rooms():
 
 # функции комнаты
 def add_room(dormitory, number, capacity):
+    db = init_firebase()
     roomData = {'number': number, 'capacity': capacity, 'occupied': 0, 'status': 'Свободна'}
     db.child('dormitory' + str(dormitory)).child('Rooms').child(roomData['number']).set(roomData)
 
@@ -53,6 +55,7 @@ def update_rooms_status():
 
 # функции студента
 def add_student(fio, phone, passport, address, educ_form, gender, dormitory):
+    db = init_firebase()
     '''добавление студента'''
     student_data = {'ФИО': fio, 'Телефон': phone, 'Паспорт': passport, 'Адрес регистрации': address,
                     'Форма обучения': educ_form, 'Пол': gender, 'Комната':'queue', 'Общежитие':dormitory}
@@ -71,28 +74,21 @@ def edit_student():
 
 def delete_student(fio):
     '''удаление студента'''
+    db = init_firebase()
     searched_mas = search_student(fio)
 
 
+
 def search_student(fio):
-    '''данные формата [(фио,№_общаги,комната,{данные}),(...)]'''
+    db = init_firebase()
+    '''отдает pyrebase obj к нему применять each()'''
     name_mas = fio.split()
     name = name_mas[0] + name_mas[1][0] + name_mas[2][0]
 
     searched_names = db.child('clients').order_by_child('ФИО').equal_to(fio).get()
-    print(searched_names.each())
+    return searched_names
 
-    # searched_students = []
-    # for dormitory in searched_mas.each():
-    #     dorm_number = dormitory.val()['number']
-    #     rooms = dormitory.val()['Rooms']
-    #     for room_ind in rooms.keys():
-    #         room = rooms[room_ind]
-    #         if 'Residents' in room.keys():
-    #             residents = room['Residents']
-    #             if name in residents.keys():
-    #                 searched_students.append((name,str(dorm_number),room_ind,residents[name]))
-    # return searched_students
+
 
 
 
@@ -160,10 +156,11 @@ if __name__ == '__main__':
     # for r in a.keys():
     #     print(a[r])
     # add_student('Romanenko V Y','88005553535','324343234','Москва','бюджет','мужской',1)
+    db = init_firebase()
+    print(search_student('Романенко Владимир Юрьевич'))
+    for elem in search_student('Романенко Владимир Юрьевич').each():
+        print(elem.val())
 
-    # search_student('Романенко Владимир Юрьевич')
-
-    print(db.child('dormitories').order_by_child('number').equal_to(1).get())
 
 
 
